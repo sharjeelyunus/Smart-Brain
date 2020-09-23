a423cea323305465feede4af89b8e4ac701feb66
sharjeelyunus <sharjeel924@gmail.com>
2020-09-24 00:15:39 +0500

    📦 NEW: Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
     </div>
   );
 }
