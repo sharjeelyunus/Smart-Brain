49b2cce91af2684c716f3bd5968b91655ebc3c20
sharjeelyunus <sharjeel924@gmail.com>
2020-09-25 16:21:44 +0500

    📦 NEW:  Logo 🛠

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/*<ImageLinkForm />
       <FaceRecognition />*/}
     </div>
   );
