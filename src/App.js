1d86b4496646accbfbf47d59939e7c851a115518
sharjeelyunus <sharjeel924@gmail.com>
2020-09-27 19:06:25 +0500

    📦 NEW: Background Particles 🛠

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 400,
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
       <Rank />
