0f73e6c14fcfc6d00e3a578db08b486a38c57f78
sharjeelyunus <sharjeel924@gmail.com>
2020-09-28 17:02:28 +0500

    📦 NEW: SearchBar Functionality

--- src/App.js
+++ src/App.js
@@ -19,6 +19,17 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,7 +39,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
         {/*<FaceRecognition />*/}
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
     return (
         <div>
             <p className='f3'>
@@ -9,7 +9,7 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' />
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
             </div>
