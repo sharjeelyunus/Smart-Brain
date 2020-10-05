26428905a4a5651ab47776b90fea3e1eee907919
sharjeelyunus <sharjeel924@gmail.com>
2020-10-06 01:08:32 +0500

    👌 IMPROVE: Detect Face Calculation

--- src/App.js
+++ src/App.js
@@ -30,7 +30,26 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -43,13 +62,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-      function (response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function (err) {
-        //there was an error
-      });
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({ imageUrl }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img alt='' src={imageUrl} width='500px' height='auto'/>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
             </div>
         </div>
     )
