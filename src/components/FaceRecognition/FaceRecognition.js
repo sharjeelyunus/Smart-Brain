5eff851a5650e3fcf1c98856a7a19d3e85aea5d9
sharjeelyunus <sharjeel924@gmail.com>
2020-10-06 00:36:03 +0500

    🐛 FIX: Image Size

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
     return (
-        <div className='center'>
-            <img alt='' src={imageUrl}/>
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img alt='' src={imageUrl} width='500px' height='auto'/>
+            </div>
         </div>
     )
 }
