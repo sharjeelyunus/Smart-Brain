154fcfeb17f665e5805538df6aa12c36c0184e29
sharjeelyunus <sharjeel924@gmail.com>
2020-10-09 20:00:00 +0500

    👌 IMPROVE: entries🛠

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -48,7 +48,7 @@ app.post('/register', (req, res) => {
         entries: 0,
         joined: new Date()
     })
-    res.json(database.users[database.users.length-1]);
+    res.json(database.users[database.users.length - 1]);
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -58,7 +58,22 @@ app.get('/profile/:id', (req, res) => {
         if (user.id === id) {
             found = true;
             return res.json(user);
-        } 
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
     })
     if (!found) {
         res.status(400).json('not found')
