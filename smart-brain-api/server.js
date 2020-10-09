568f60102aefad833c1b11cdfe56ceae93c1f29a
sharjeelyunus <sharjeel924@gmail.com>
2020-10-09 18:23:48 +0500

    📦 NEW: Signin ✅

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res)=> {
-    res.send('this is working')
+    res.send('this is working');
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
 })
 
 app.listen(3000, () => {
