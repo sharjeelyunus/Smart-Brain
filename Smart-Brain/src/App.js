2c824c41360efbb99be1104f694c77eec6d07bdc
sharjeelyunus <sharjeel924@gmail.com>
2020-10-11 00:38:47 +0500

    👌 IMPROVE: Connecting Register ✅

--- Smart-Brain/src/App.js
+++ Smart-Brain/src/App.js
@@ -34,8 +34,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -71,9 +90,9 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState({ isSignedIn: false })
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({ isSignedIn: true })
     }
     this.setState({ route: route });
   }
@@ -97,11 +116,11 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
           : (
-            route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange} /> 
-            : <Register onRouteChange={this.onRouteChange} />
+            route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
-          
+
 
         }
       </div>
--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -48,7 +48,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
