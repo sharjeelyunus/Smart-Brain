3983a9df15bb44e6001d577fbf878746ac625c5a
sharjeelyunus <sharjeel924@gmail.com>
2020-10-12 22:58:15 +0500

    👌 IMPROVE: db Register

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 var knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
@@ -13,8 +13,6 @@ const postgres = knex({
         database: 'smart-brain'
     }
 });
-
-console.log(postgres.select('*').from('users'));;
 
 const app = express();
 
@@ -57,13 +55,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users').insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    })
+    }).then(console.log)
     res.json(database.users[database.users.length - 1]);
 })
 
