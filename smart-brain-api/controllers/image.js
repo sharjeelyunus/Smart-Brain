9f7b3fd1647be10e02dbfe38ac3da3e9ac799082
sharjeelyunus <sharjeel924@gmail.com>
2020-10-13 22:28:30 +0500

    👌 IMPROVE: Adding Controllers => Code Clean up

---
+++ smart-brain-api/controllers/image.js
@@ -0,0 +1,14 @@
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage
+}
---
+++ smart-brain-api/controllers/signin.js
@@ -0,0 +1,22 @@
+const handleSignin =(req, res, db, bcrypt) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
+}
+
+module.exports = {
+    handleSignin: handleSignin
+}
