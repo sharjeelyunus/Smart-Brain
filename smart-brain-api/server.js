5297c2e527c9fa66c5782e9be2daabd6122659e0
sharjeelyunus <sharjeel924@gmail.com>
2020-10-14 17:38:11 +0500

    changing response

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -46,7 +46,7 @@ const database = {
 
 app.use(cors())
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
