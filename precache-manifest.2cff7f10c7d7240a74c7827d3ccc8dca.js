f4a65b37b718d36bcb109cde8ebd1d2f65ee9e08
sharjeelyunus <sharjeel924@gmail.com>
2020-10-15 03:53:04 +0500

    Updates

---
+++ precache-manifest.2cff7f10c7d7240a74c7827d3ccc8dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eb29c122c5ea0e207d6d2e1fe32b19",
+    "url": "/Smart-Brain/index.html"
+  },
+  {
+    "revision": "282c712b5ef836d532bf",
+    "url": "/Smart-Brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e02ccb0782bbd9980fb8",
+    "url": "/Smart-Brain/static/css/main.24a7733f.chunk.css"
+  },
+  {
+    "revision": "282c712b5ef836d532bf",
+    "url": "/Smart-Brain/static/js/2.02875cfa.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Smart-Brain/static/js/2.02875cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02ccb0782bbd9980fb8",
+    "url": "/Smart-Brain/static/js/main.957d899f.chunk.js"
+  },
+  {
+    "revision": "edda819a6ce46a7a617e",
+    "url": "/Smart-Brain/static/js/runtime-main.7bba5e85.js"
+  }
+]);
